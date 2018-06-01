1ffc01fe23c94d289953d26edbc751529fb590f0
Mark Cipolla <mark@markcipolla.com>
2018-06-01 10:53:18 +1000

    Simplify FileDownload to a function rather than a class

--- .storybook/FileDownload/index.jsx
+++ .storybook/FileDownload/index.jsx
@@ -3,20 +3,17 @@ import PropTypes from "prop-types";
 import styles from './styles.css';
 import file from './file.svg';
 
-class FileDownload extends Component {
-  render() {
-    return (
-      <div className={styles.fileDownload}>
-        <div className={styles.file}>
-          <img src={file} />
-          <span className={styles.fileType}>{this.props.fileType}</span>
-        </div>
-        <a href={this.props.file}>{this.props.label}</a>
+function FileDownload(props) {
+  return (
+    <div className={styles.fileDownload}>
+      <div className={styles.file}>
+        <img src={file} />
+        <span className={styles.fileType}>{props.fileType}</span>
       </div>
-    );
-  }
-}
-
+      <a href={props.file}>{props.label}</a>
+    </div>
+  )
+};
 
 FileDownload.defaultProps = {
   file: undefined,
