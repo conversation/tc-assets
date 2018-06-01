b8739f82ad1de3a03d1f8f22e19219c41d44931c
Mark Cipolla <mark@markcipolla.com>
2018-06-01 15:13:46 +1000

    Add a SearchBox component

---
+++ Components/SearchBox/README.md
@@ -0,0 +1,11 @@
+# SearchBox
+
+The SearchBox provides a text input that throttles it's `onChange` event
+which we can use to make async calls for data.
+
+## Properties
+
+ propName    | propType | defaultValue | isRequired
+-------------|----------|--------------|:----------:
+ placeholder | String   | ""           |
+ onChange    | Function |              | ✓
---
+++ Components/SearchBox/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { throttle } from "underscore";
+import styles from "./styles.css";
+
+export default class SearchBox extends React.Component {
+  static submit(e) {
+    e.preventDefault();
+  }
+
+  constructor(props) {
+    super(props);
+    this.throttledNotify = throttle(this.notifySearchChanged, 500, { leading: false });
+    this.keyup = this.keyup.bind(this);
+  }
+
+  keyup(e) {
+    this.throttledNotify(e.target.value);
+  }
+
+  notifySearchChanged(search) {
+    this.props.onChange(search);
+  }
+
+  render() {
+    return (
+      <form onSubmit={SearchBox.submit}>
+        <input
+          type="text"
+          name="q"
+          onKeyUp={this.keyup}
+          placeholder={this.props.placeholder}
+          className={styles.input}
+        />
+      </form>
+    );
+  }
+}
+
+SearchBox.defaultProps = {
+  placeholder: "",
+};
+
+SearchBox.propTypes = {
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
