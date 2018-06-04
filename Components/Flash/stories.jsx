f18649695a6a2afc84f88a024a7557220f8802a3
Mark Cipolla <mark@markcipolla.com>
2018-06-04 12:12:03 +1000

    Restructure the <Flash> stories as changeable with Knobs

--- Components/Flash/stories.jsx
+++ Components/Flash/stories.jsx
@@ -4,26 +4,25 @@ import { doc } from 'storybook-readme';
 import README from './README.md';
 import Flash from "./index";
 
-import { withKnobs, boolean, text } from '@storybook/addon-knobs/react';
+import { withKnobs, select, boolean, text } from '@storybook/addon-knobs/react';
 
 storiesOf('Flash', module).add('Overview', doc(README));
 
 const stories = storiesOf('Flash', module);
 stories.addDecorator(withKnobs);
-// We need to display these as separate stories as each flash message
-// has `position: fixed`, and we can't display these on one screen
-// without them overlapping each other.
 
-stories.add("Notice", () => (
-  <Flash message={text("Message", "A flash message")} type="notice" />
-))
-.add("Warning", () => (
-  <Flash message={text("Message", "A flash message")} type="warning" />
-))
-.add("Alert", () => (
-  <Flash message={text("Message", "A flash message")} type="alert" />
-))
-.add("Fade", () => (
-  <Flash message={text("Message", "A flash message")} fade={boolean('Fade', false)} />
-));;
+const label = 'Type';
+
+const options = {
+  defaultType: '',
+  notice: 'notice',
+  warning: 'warning',
+  alert: 'alert',
+};
+
+const defaultValue = 'defaultType';
+
+stories.add("Display", () => (
+  <Flash message={text("Message", "A flash message")} fade={boolean('Fade', false)} type={select(label, options, defaultValue, "Flash Notices")} />
+));
 
